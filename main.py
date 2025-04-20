3ed5b26ee503c51982bd1085a1347c233cff4baf
Nick Leocadio <nick_leo@msn.com>
2025-04-20 00:00:55 -0400

    Created intereact perceive class for simulation

--- main.py
+++ main.py
@@ -8,35 +8,101 @@ import subprocess
 import sys
 import signal
 import atexit
+import os
 
-filename = "ft_data.csv"
-ft_names = ["fx", "fy", "fz", "tx", "ty", "tz"]
-num_joints = 7
-fieldnames = [f"{name}_{j}" for j in range(1, num_joints+1) for name in ft_names]
-
+# -----------------------------------------------------------------------------------------------------------
 class InteractPerceive:
-    def __init__(self, robot):
-        self.robot = robot
-        self.joint_indices = [i for i in range(7)]  # Joint indices of the robot
-        self.p = p
+    def __init__(self):
+        super().__init__()
+        self.robot = None
+        self.filename = "ft_data.csv"
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        self.ft_names = ["fx", "fy", "fz", "tx", "ty", "tz"]
+        self.num_joints = 7
+        self.fieldnames = [f"{name}_{j}" for j in range(1, self.num_joints+1) for name in self.ft_names]
+        self.ft = {
+            f"{name}_{j}": 0
+            for j in range(1, self.num_joints+1)
+            for name in self.ft_names
+        }
+        
+    def init_sim(self):
+        p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0, 0, -9.81)
+        p.setRealTimeSimulation(1)
+        p.resetDebugVisualizerCamera(cameraDistance=1.2,
+                                          cameraYaw=50,
+                                          cameraPitch=-30,
+                                          cameraTargetPosition=[0.5, 0, 0.2])
+        p.loadURDF("plane.urdf") # ground plane
 
-    def get_joint_states(self):
-        joint_states = self.p.getJointStates(self.robot, self.joint_indices)
-        return joint_states
+        urdf_dir = os.path.join(self.path, "panda_with_sensor.urdf")
+        print(f"Loading URDF from: {urdf_dir}")
+        self.robot = p.loadURDF(urdf_dir,
+                                basePosition=[0, 0, 0],
+                                baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
+                                useFixedBase=True)
+        self.create_object()
+        
+    def create_object(self):
+        # create a red box at (0.5, 0, 0.05)
+        box_half_extents = [0.05]*3
+        box_col = p.createCollisionShape(p.GEOM_BOX, halfExtents=box_half_extents)
+        box_vis = p.createVisualShape(p.GEOM_BOX, halfExtents=box_half_extents,
+                                            rgbaColor=[1, 0, 0, 1])
+        p.createMultiBody(baseMass=1,
+                                baseCollisionShapeIndex=box_col,
+                                baseVisualShapeIndex=box_vis,
+                                basePosition=[0.5, 0, 0.05])
 
-def main():    
-    # -----------------------------------------------------------------------------------------------------------
-    # Initialize plot file & start up plotter, register cleanup
-    # -----------------------------------------------------------------------------------------------------------
-    ft = {
-        f"{name}_{j}": 0
-        for j in range(1, num_joints+1)
-        for name in ft_names
-    }
-    initialize_plot_file()
-    
+    # Initialize CSV file with headers, overwrites/deletes existing file with the same name
+    def initialize_plot_file(self, filename: str = None, fieldnames: list = None):
+        if filename is None:
+            filename = self.filename
+        if fieldnames is None:
+            fieldnames = self.fieldnames
+            
+        with open(filename, "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+
+    # Write joint index & force/torque readings to CSV file
+    def write_to_csv(self, data_dict: dict, filename: str = None, fieldnames: list = None):
+        if filename is None:
+            filename = self.filename
+        if fieldnames is None:
+            fieldnames = self.fieldnames
+            
+        with open(filename, "a", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writerow(data_dict)
+            
+    def get_forces(self):
+        for joint in range(0, self.num_joints):
+            _, _, forces, _ = p.getJointState(self.robot, joint)
+            # fx, fy, fz, mx, my, mz = forces
+            for name, val in zip(self.ft_names, forces):
+                self.ft[f"{name}_{joint + 1}"] = val
+        
+        return self.ft
+
+# -----------------------------------------------------------------------------------------------------------
+
+'''
+start_plotter
+
+Starts the plotter.py script that plots forces on each joint
+Registers cleanup function on exit to kill the plot process
+'''
+def start_plotter():
+    plotter_path = os.path.join(os.path.dirname(__file__), "plotter.py")
+    if not os.path.exists(plotter_path):
+        print(f"Plotter script not found at {plotter_path}.")
+        sys.exit(1)
+        
     plotter = subprocess.Popen(
-        [sys.executable, "plotter.py"],
+        [sys.executable, plotter_path],
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
@@ -57,35 +123,14 @@ def main():
     # 4) register for normal program exit
     atexit.register(cleanup)
     
-    # -----------------------------------------------------------------------------------------------------------
-    # Load the simulation and robot
-    # -----------------------------------------------------------------------------------------------------------
-    p.connect(p.GUI)
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    
-    p.setGravity(0, 0, -9.81)
-    p.setRealTimeSimulation(1)
-    p.resetDebugVisualizerCamera(cameraDistance=1.2,
-                                 cameraYaw=50,
-                                 cameraPitch=-30,
-                                 cameraTargetPosition=[0.5, 0, 0.2])
+# -----------------------------------------------------------------------------------------------------------
 
-    # ground plane
-    p.loadURDF("plane.urdf")
-
-    robot = p.loadURDF("panda_with_sensor.urdf",
-                       basePosition=[0, 0, 0],
-                       baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
-                       useFixedBase=True)
-
-    # create a red box at (0.5, 0, 0.05)
-    box_col = p.createCollisionShape(p.GEOM_BOX, halfExtents=[0.05]*3)
-    box_vis = p.createVisualShape(p.GEOM_BOX, halfExtents=[0.05]*3,
-                                  rgbaColor=[1, 0, 0, 1])
-    p.createMultiBody(baseMass=1,
-                      baseCollisionShapeIndex=box_col,
-                      baseVisualShapeIndex=box_vis,
-                      basePosition=[0.5, 0, 0.05])
+def main():    
+    # Initialize the simulation
+    ip = InteractPerceive()
+    ip.init_sim()
+    ip.initialize_plot_file()
+    start_plotter()
 
     # -----------------------------------------------------------------------------------------------------------
     # Indices on Franka Panda that we care about
@@ -101,7 +146,7 @@ def main():
     # Initial position
     # -----------------------------------------------------------------------------------------------------------
     above_pos = [0.5, 0, 0.3]
-    ik_vals = p.calculateInverseKinematics(robot,
+    ik_vals = p.calculateInverseKinematics(ip.robot,
                                            ee_link_index,
                                            above_pos,
                                            down_ori,
@@ -109,8 +154,8 @@ def main():
                                            residualThreshold=1e-5)
     # snap to that pose before we start moving
     for i in range(7):
-        p.resetJointState(robot, i, ik_vals[i])
-        p.enableJointForceTorqueSensor(robot, i, 1)
+        p.resetJointState(ip.robot, i, ik_vals[i])
+        p.enableJointForceTorqueSensor(ip.robot, i, 1)
     time.sleep(1)  # let things settle
 
     print("Lowering into the cube…")
@@ -120,19 +165,19 @@ def main():
     # ———————————
     for z in [0.15, 0.1, 0.05]:  # descending heights
         target = [0.5, 0, z]
-        ik_vals = p.calculateInverseKinematics(robot,
+        ik_vals = p.calculateInverseKinematics(ip.robot,
                                                ee_link_index,
                                                target,
                                                down_ori)
         # command the arm joints
         for i in range(7):
-            p.setJointMotorControl2(robot, i,
+            p.setJointMotorControl2(ip.robot, i,
                                     controlMode=p.POSITION_CONTROL,
                                     targetPosition=ik_vals[i],
                                     force=200)
         # keep gripper half‑closed
         for j in finger_joints:
-            p.setJointMotorControl2(robot, j,
+            p.setJointMotorControl2(ip.robot, j,
                                     controlMode=p.POSITION_CONTROL,
                                     targetPosition=0.04,
                                     force=50)
@@ -145,34 +190,13 @@ def main():
     # -----------------------------------------------------------------------------------------------------------
     try:
         while True:
-            for joint in range(0, num_joints):
-                _, _, forces, _ = p.getJointState(robot, joint)
-                # fx, fy, fz, mx, my, mz = forces
-                for name, val in zip(ft_names, forces):
-                    ft[f"{name}_{joint + 1}"] = val
-            
-            write_to_csv(data_dict=ft)
-            
+            ft = ip.get_forces()
+            ip.write_to_csv(data_dict=ft)
             time.sleep(0.5)
     except KeyboardInterrupt:
         pass
     finally:
         p.disconnect()
-        plotter.terminate()
-        plotter.wait()
-
-# Initialize CSV file with headers
-# Overwrites/deletes existing file with the same name
-def initialize_plot_file(filename: str = filename, fieldnames: list = fieldnames):
-    with open(filename, "w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writeheader()
-
-# Write joint index & force/torque readings to CSV file
-def write_to_csv(data_dict: dict, filename: str = filename, fieldnames: list = fieldnames):
-    with open(filename, "a", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writerow(data_dict)
         
 if __name__ == "__main__":
     main()
